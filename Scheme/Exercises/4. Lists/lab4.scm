e66e49c3a83ba30697d781c9a55d8856f4f0e761
Stefania Tsvetkova <nia_flo@abv.bg>
2022-11-30 00:21:28 +0200

    Added scheme excercises

---
+++ Scheme/Exercises/4. Lists/lab4.scm
@@ -0,0 +1,131 @@
+(define (accumulate op nv a b term next)
+  (if (> a b)
+      nv
+      (op (term a) (accumulate op nv (next a) b term next))))
+
+;; count-divisors n - намира броя делители на числото n чрез accumulate
+(define (count-divisors n)
+  (define (1+ x) (+ 1 x))
+  (define (divisor? x) (if (= (remainder n x) 0) 1 0))
+
+  (if (< n 0)
+      (+ 1 (count-divisors (- n)))
+      (accumulate + 0 1 n divisor? 1+)))
+;; задачи с наредени двойки
+;; (x . y) - рационално число, х - числител, у - знаменател
+(define (make-rational x y)
+  (cons x y))
+
+(define (nominator a)
+  (car a))
+
+(define (my-denominator a)
+  (cdr a))
+
+(define (sum-rationals a b)
+  (/ (+ (* (nominator a) (my-denominator b)) (* (my-denominator a) (nominator b))) (* (my-denominator a) (my-denominator b))))
+
+(define (substract-rationals a b)
+  (/ (- (* (nominator a) (my-denominator b)) (* (my-denominator a) (nominator b))) (* (my-denominator a) (my-denominator b))))
+
+(define (mult-rationals a b)
+  (/ (* (nominator a) (nominator b)) (* (my-denominator a) (my-denominator b))))
+
+(define (div-rationals a b)
+  (mult-rationals a (cons (my-denominator b) (nominator b))))
+
+(define (mult-rationals2 a b)
+  (let (
+        (nom (* (nominator a) (nominator b)))
+        (denom(* (my-denominator a) (my-denominator b))))
+    (make-rational nom denom)))
+
+;; задачи със списъци
+(define (sum-elements l)
+  (if (null? l)
+      0
+      (+ 1 (sum-elements (cdr l)))))
+
+(define (member? x l)
+  (cond
+    ((null? l) #f)
+    ((= (car l) x) #t)
+    (else (member? x (cdr l)))))
+
+
+;; = - сравнява числа
+;; eqv? - сравнява прости типове
+;; equal? - сравнява списъци
+;; eq? - сравнява по адреси, имплементацията е различна в зависимост от средата, не го използваме
+
+;; my-lenght l - намира дължината на списъка
+(define (my-lenght l)
+  (if (null? l)
+      0
+      (+ 1 (my-lenght (cdr l)))))
+
+;; n-th l n - намира n-тия елемент на l
+(define (n-th l n)
+  (cond
+    ((or (< n 1) (null? l)) 'error)
+    ((= n 1) (car l))
+    (else (n-th (cdr l) (- n 1)))))
+
+;; count-occurs x l - намира броя на среюанията на x в l
+(define (count-occurs x l)
+  (cond
+    ((null? l) 0)
+    ((eqv? x (car l)) (+ 1 (count-occurs x (cdr l))))
+    (else (count-occurs x (cdr l)))))
+
+;; my-append l1 l2 - залепя l1 и l2
+(define (my-append l1 l2)
+  (if (null? l1)
+      l2
+      (cons (car l1) (my-append (cdr l1) l2))))
+
+;; push-back x l - залепя x в края на l
+(define (push-back x l)
+  (my-append l (cons x '())))
+
+;; take n l - връща първите n елемента на l
+(define (take n l)
+  (if (or (= n 0) (null? l))
+      '()
+      (cons (car l) (take (- n 1) (cdr l)))))
+
+;; drop n l - подсписък без първите n елемента на l
+(define (drop n l)
+  (if (or (= n 0) (null? l))
+      l
+      (drop (- n 1) (cdr l))))
+
+;; remove x l - маха първото срещане на x в l
+(define (remove x l)
+  (cond
+    ((null? l) l)
+    ((eqv? x (car l)) (cdr l))
+    (else (cons (car l) (remove x (cdr l))))))
+
+;; remove-all x l - маха всички срещания на x в l
+(define (remove-all x l)
+  (cond
+    ((null? l) l)
+    ((eqv? x (car l)) (remove-all x (cdr l)))
+    (else (cons (car l) (remove-all x (cdr l))))))
+
+;; reverse l - обръща реда на елементите на l
+
+
+;; my-map f l - прилага функцията f върху елементите на l
+(define (my-map f l)
+  (if (null? l)
+      l
+      (cons (f (car l)) (my-map f (cdr l)))))
+
+;; filter p l - връща само елементите от l, които отговарят на p
+(define (filter p l)
+  (cond
+    ((null? l) l)
+    ((p (car l)) (cons (car l) (filter p (cdr l))))
+    (else (filter p (cdr l)))))
---
+++ Scheme/Exercises/6. Matrices ans exam preparation/test2021-2022.scm
@@ -0,0 +1,84 @@
+; Problem1
+
+(define (done? n)
+  (define (dividers-sum n i)
+    (cond
+      ((= i n) 0)
+      ((= (remainder n i) 0) (+ i (dividers-sum n (+ i 1))))
+      (else (dividers-sum n (+ i 1)))))
+  
+  (= n (- (dividers-sum n 1) 2)))
+
+
+
+(define (sum-almost-done a b)
+  (define (closest-done i end n res)
+    (cond
+      ((> i end) res)
+      ((and (done? i) (or (= res -1) (> (abs (- res n)) (abs (- i n))))) (closest-done (+ i 1) end n i))
+      (else (closest-done (+ i 1) end n res))))
+  
+  (define (sum-almost-done a b i)
+    (cond
+      ((> i b) 0)
+      ((done? i) (+ i (sum-almost-done a b (+ i 1))))
+      ((> (min (- i a) (- b i)) (abs (- i (closest-done a b i -1)))) (+ i (sum-almost-done a b (+ i 1))))
+      (else (sum-almost-done a b (+ i 1)))))
+
+  (sum-almost-done a b a))
+
+; Problem 2
+
+(define (run-machine l)
+  (define (pair-op f n s)
+    (if (or (null? s) (= n 0))
+        s
+        (let (
+              (first (car s))
+              (second (cadr s))
+              (tail (cddr s)))
+          (if (and (number? first) (number? second))
+              (pair-op f (- n 1) (cons (f first second) tail))
+              s))))
+  
+  (define (run-machine l stack)
+    (if (null? l)
+        stack
+        (let (
+              (head (car l))
+              (tail (cdr l)))
+          (cond
+            ((or (number? head) (symbol? head)) (run-machine tail (cons head stack)))
+            ((procedure? head) (run-machine tail (map (lambda (x) (if (number? x) (head x) x)) stack)))
+            ((and (pair? head) (procedure? (car head)) (number? (cdr head))) (run-machine tail (pair-op (car head) (cdr head) stack)))
+            (else (run-machine tail stack))))))
+
+  (run-machine l '()))
+
+; Problem 3
+
+
+
+(define (is-major? ll)
+  (define (smaller? a b)
+    (if (null? a)
+        #t
+        (and (<= (car a) (car b)) (smaller? (cdr a) (cdr b)))))
+
+  (define (major? a b)
+    (cond
+      ((> (length a) (length b)) #f)
+      ((smaller? a b) #t)
+      (else (major? a (cdr b)))))
+
+  (define (is-major? last-l ll)
+    (if (null? ll)
+        #t
+        (let (
+              (head (car ll))
+              (tail (cdr ll)))
+          (if (major? last-l head)
+              (is-major? head tail)
+              #f))))
+
+  (is-major? (car ll) (cdr ll)))  
